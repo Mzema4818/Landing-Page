6424c5a5e4595be8c07bf49c65be1e8d805f083a
Matthew Zeman <mzeman4818@baysidehighschool.org>
2024-07-25 23:17:33 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+$(function(){
+	var inputs = $('.input');
+	var paras = $('.description-flex-container').find('p');
+	inputs.click(function(){
+		var t = $(this),
+				ind = t.index(),
+				matchedPara = paras.eq(ind);
+		
+		t.add(matchedPara).addClass('active');
+		inputs.not(t).add(paras.not(matchedPara)).removeClass('active');
+	});
+});
+
+function updateProgressBar(){
+  const {scrollTop, scrollHeight} = document.documentElement;
+  const scrollPercent = scrollTop / (scrollHeight - window.innerHeight) * 100 + '%';
+  document.querySelector('#progress-bar').style.setProperty('--progress', scrollPercent);
+}
+
+document.addEventListener('scroll', updateProgressBar);
